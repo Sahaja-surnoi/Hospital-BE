7958c6f31df5ef6f5dca3028fa39aa0c3525f30f
Sahaja-surnoi <Sahajagarrepally@surnoi.com>
2024-10-10 15:54:21 +0530

    Update HospitalAppointmentApplication.java

--- src/main/java/com/example/DoctorAppointment/HospitalAppointmentApplication.java
+++ src/main/java/com/example/DoctorAppointment/HospitalAppointmentApplication.java
@@ -19,7 +19,7 @@ public class HospitalAppointmentApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://13.229.133.101") // Frontend URL
+						.allowedOrigins("http://ec2-52-77-215-15.ap-southeast-1.compute.amazonaws.com") // Frontend URL
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Ensure OPTIONS is included for preflight requests
 						.allowedHeaders("*") // Allows all headers
 						.allowCredentials(true); // Allows credentials if needed
